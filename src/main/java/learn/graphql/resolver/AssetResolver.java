ae5a67bb6da92d07685f8c9b183ce34257a1033e
eric <wanghuanxi@gmail.com>
2021-10-03 22:27:44 +0800

    add exception handler

--- src/main/java/learn/graphql/resolver/AssetResolver.java
+++ src/main/java/learn/graphql/resolver/AssetResolver.java
@@ -3,7 +3,9 @@ package learn.graphql.resolver;
 import graphql.kickstart.tools.GraphQLResolver;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import learn.graphql.domain.Asset;
 import learn.graphql.domain.BankAccount;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +14,29 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class AssetResolver implements GraphQLResolver<BankAccount> {
+  private final ExecutorService executorService = Executors.newFixedThreadPool(
+      Runtime.getRuntime().availableProcessors()
+  );
+//
+  public CompletableFuture<List<Asset>> assets(BankAccount bankAccount) {
+    return CompletableFuture.supplyAsync(
+        () -> {
+          log.info("Getting assets for bank account id {}", bankAccount.getId());
+          List assets = new ArrayList();
+          return assets;
+          },
+        executorService
+    );
+  }
 
-  public List<Asset> assets(BankAccount bankAccount){
-    List assets = new ArrayList();
-    return assets;
+  public CompletableFuture<List<Asset>> getAssets(BankAccount bankAccount){
+    return CompletableFuture.supplyAsync(
+        ()->{
+          log.info("Getting getAssets for bank account id {}", bankAccount.getId());
+          List assets = new ArrayList();
+          return assets;
+        },
+                executorService
+    );
   }
 }
